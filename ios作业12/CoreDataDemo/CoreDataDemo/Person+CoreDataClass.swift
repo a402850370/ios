3a018dc82a034a1b1b62ea4a6c0aa2df2d7ef48c
a402850370 <402850370@qq.com>
2018-12-18 14:23:31 +0800

    some msg

---
+++ ios作业12/CoreDataDemo/CoreDataDemo/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by xj on 18/11/18.
+//  Copyright © 2018年 xj. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Person: NSManagedObject {
+
+}
